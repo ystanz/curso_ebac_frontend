02834086785c600034d01c80b35f359445202703
Yuri Stanzani <ystanzani@gmail.com>
2023-07-17 17:07:05 -0300

    Added task to compile sass

--- exercicio_gulp/gulpfile.js
+++ exercicio_gulp/gulpfile.js
@@ -1,8 +1,18 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
 const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
-const sourcemaps = require('gulp-sourcemaps');
+
+function sassCompile() {
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'));
+}
 
 
-
+exports.sassCompile = sassCompile;
