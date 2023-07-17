8f3157f4afc9fa9c494a566e4da976e661f5e08b
Yuri Stanzani <ystanzani@gmail.com>
2023-07-17 16:23:19 -0300

    Initial commit

---
+++ exercicio_gulp/gulpfile.js
@@ -0,0 +1,8 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
+
+
+
