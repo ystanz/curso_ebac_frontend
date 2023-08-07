0bd3c81c78a4f58f3a8f0e1a8a6448c7b3b22567
Yuri Stanzani <ystanzani@gmail.com>
2023-08-07 00:08:56 -0300

    Created tasks for compiling LESS and compressing JS code, exercise completed

--- exercicio_grunt/gruntfile.js
+++ exercicio_grunt/gruntfile.js
@@ -1,5 +1,40 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'dev/styles/main.css': 'src/styles/main.less'
+                }
+            },
+            production: {
+                options: {
+                    compress: true,
+                },
+                files: {
+                    'dist/styles/main.min.css': 'src/styles/main.less'
+                }
+            }
+        },
+        uglify: {
+            target: {
+                files: {
+                    'dist/scripts/main.min.js' : 'src/scripts/main.js'
+                }
+            }
+        },
+        watch: {
+            less: {
+                files: ['src/styles/**/*.less'],
+                tasks: ['less:development']
+            }
+        }
     })
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['less:production', 'uglify']);
 }
